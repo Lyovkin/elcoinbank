a6bbdebe08b2e9c606b6f82d501ca616c087951b
Lyovkin <anatoliymolinari@gmail.com>
2016-07-20 17:57:32 +0300

    fix

--- resources/views/deposits/index.blade.php
+++ resources/views/deposits/index.blade.php
@@ -17,57 +17,59 @@
         <div class="row">
             <div class="col-md-12" >
                 <h2 class="header">Мои вклады</h2>
-                <table class="table table-bordered table-responsive" style="margin-top: 30px;">
-                    <thead>
-                    <tr class="head">
-                        <th>Тип вклада</th>
-                        <th>Дней</th>
-                        <th>Процент</th>
-                        <th>Сумма вклада EL</th>
-                        <th>Сумма для вывода EL</th>
-                        <th>Статус</th>
-                        <th>Истечение времени вклада</th>
-                        <th>Создан</th>
-                        <th>Вывести</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @if(count($deposits))
-                    @foreach($deposits as $deposit)
-                        <tr>
-                            <td class="success">{{ $deposit->plan->type->name }}</td>
-                            <td class="success">{{ $deposit->days }}</td>
-                            <td class="success">{{ $deposit->percent }} %</td>
-                            <td class="success">{{ $deposit->amount }}</td>
-                            <td class="success">{{ $deposit->total }}</td>
-                            @if($deposit->conclusion > new DateTime() )
-                                <td class="success">Ожидайте...</td>
-                            @else
-                                <td class="success">Готов к выводу</td>
-                            @endif
-                            <td class="success">{{ $deposit->status == 0 ? $deposit->conclusion->format('d.m.Y H:i:s') :
-                            "Выведен в ".$deposit->conclusion->format('d.m.Y H:i:s')}}</td>
-                            <td class="success">{{ $deposit->created_at->diffForHumans() }}</td>
-                            <td class="success">
-                                <form action="/profile/deposits/{{ $deposit->id }}" method="post">
-                                    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-                                    <input type="hidden" name="status" value="1" />
-                                    @if($deposit->conclusion <= new DateTime())
-                                        <button type="submit" class="btn btn-default">Вывести на баланс</button>
-                                    @elseif($deposit->status == 1)
-                                        <button type="button" class="btn btn-default" disabled>Выведен</button>
-                                    @else
-                                        <button type="submit" class="btn btn-default" disabled>Вывести на баланс</button>
-                                    @endif
-                                </form>
-                            </td>
+                <div class="table-responsive">
+                    <table class="table table-bordered" style="margin-top: 30px;">
+                        <thead>
+                        <tr class="head">
+                            <th>Тип вклада</th>
+                            <th>Дней</th>
+                            <th>Процент</th>
+                            <th>Сумма вклада EL</th>
+                            <th>Сумма для вывода EL</th>
+                            <th>Статус</th>
+                            <th>Истечение времени вклада</th>
+                            <th>Создан</th>
+                            <th>Вывести</th>
                         </tr>
-                    @endforeach
-                    @else
-                        <p class="h3 text-center">Нет вкладов</p>
-                    @endif
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        @if(count($deposits))
+                            @foreach($deposits as $deposit)
+                                <tr>
+                                    <td class="success">{{ $deposit->plan->type->name }}</td>
+                                    <td class="success">{{ $deposit->days }}</td>
+                                    <td class="success">{{ $deposit->percent }} %</td>
+                                    <td class="success">{{ $deposit->amount }}</td>
+                                    <td class="success">{{ $deposit->total }}</td>
+                                    @if($deposit->conclusion > new DateTime() )
+                                        <td class="success">Ожидайте...</td>
+                                    @else
+                                        <td class="success">Готов к выводу</td>
+                                    @endif
+                                    <td class="success">{{ $deposit->status == 0 ? $deposit->conclusion->format('d.m.Y H:i:s') :
+                            "Выведен в ".$deposit->conclusion->format('d.m.Y H:i:s')}}</td>
+                                    <td class="success">{{ $deposit->created_at->diffForHumans() }}</td>
+                                    <td class="success">
+                                        <form action="/profile/deposits/{{ $deposit->id }}" method="post">
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                                            <input type="hidden" name="status" value="1" />
+                                            @if($deposit->conclusion <= new DateTime())
+                                                <button type="submit" class="btn btn-default">Вывести на баланс</button>
+                                            @elseif($deposit->status == 1)
+                                                <button type="button" class="btn btn-default" disabled>Выведен</button>
+                                            @else
+                                                <button type="submit" class="btn btn-default" disabled>Вывести на баланс</button>
+                                            @endif
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <p class="h3 text-center">Нет вкладов</p>
+                        @endif
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -17,84 +17,90 @@
         <div class="row">
             <div class="col-md-12" >
                 <h2 class="header">Покупка EL / Перевод EL</h2>
-                <table class="table table-bordered table-responsive" style="margin-top: 30px;">
-                    <thead>
-                    <tr class="head">
-                        <th>Тип операции</th>
-                        <th>Валюта</th>
-                        <th>Со счета</th>
-                        <th>По курсу</th>
-                        <th>Сумма</th>
-                        <th>Сумма EL</th>
-                        <th>Примечание</th>
-                        <th>В дов.управление</th>
-                        <th>Одобрена</th>
-                        <th>Создана</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @if(count($purchases) > 0)
-                    @foreach($purchases as $purchase)
-                        <tr>
-                            <td class="success">{{ $purchase->type->name }}</td>
-                            <td class="success">{{ $purchase->type_id != 3 ? $purchase->currency->name : '-' }}</td>
-                            <td class="success">{{ $purchase->payment ? $purchase->payment : '-' }}</td>
-                            <td class="success">{{ $purchase->type_id != 3 ? $purchase->course : '-'}}</td>
-                            <td class="success">{{ $purchase->amount }}</td>
-                            <td class="success">{{ $purchase->total }}</td>
-                            <td class="success">{{ $purchase->message ? $purchase->message : '-' }}</td>
-                            <td class="success">{{ $purchase->status_trust ? 'Дa' : 'Нет' }}</td>
-                            <td class="success">{{ $purchase->status_admin ? 'Да' : 'Нет' }}</td>
-                            <td class="success">{{ $purchase->created_at->diffForHumans() }}</td>
+                <div class="table-responsive">
+                    <table class="table table-bordered" style="margin-top: 30px;">
+                        <thead>
+                        <tr class="head">
+                            <th>Тип операции</th>
+                            <th>Валюта</th>
+                            <th>Со счета</th>
+                            <th>По курсу</th>
+                            <th>Сумма</th>
+                            <th>Сумма EL</th>
+                            <th>Примечание</th>
+                            <th>В дов.управление</th>
+                            <th>Одобрена</th>
+                            <th>Создана</th>
                         </tr>
-                    @endforeach
-                    @else
-                        <p class="h3 text-center">Нет покупок</p>
-                    @endif
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        @if(count($purchases) > 0)
+                            @foreach($purchases as $purchase)
+                                <tr>
+                                    <td class="success">{{ $purchase->type->name }}</td>
+                                    <td class="success">{{ $purchase->type_id != 3 ? $purchase->currency->name : '-' }}</td>
+                                    <td class="success">{{ $purchase->payment ? $purchase->payment : '-' }}</td>
+                                    <td class="success">{{ $purchase->type_id != 3 ? $purchase->course : '-'}}</td>
+                                    <td class="success">{{ $purchase->amount }}</td>
+                                    <td class="success">{{ $purchase->total }}</td>
+                                    <td class="success">{{ $purchase->message ? $purchase->message : '-' }}</td>
+                                    <td class="success">{{ $purchase->status_trust ? 'Дa' : 'Нет' }}</td>
+                                    <td class="success">{{ $purchase->status_admin ? 'Да' : 'Нет' }}</td>
+                                    <td class="success">{{ $purchase->created_at->diffForHumans() }}</td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <p class="h3 text-center">Нет покупок</p>
+                        @endif
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         </div>
 
         <div class="row">
             <div class="col-md-12">
                 <h2 class="header">Мои вклады</h2>
-                <table class="table table-bordered table-responsive" style="margin-top: 30px;">
-                    <thead>
-                    <tr class="head">
-                        <th>Тип вклада</th>
-                        <th>Дней</th>
-                        <th>Процент</th>
-                        <th>Сумма вклада EL</th>
-                        <th>Сумма для вывода EL</th>
-                        <th>Статус</th>
-                        <th>Истечение времени вклада</th>
-                        <th>Создан</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @if(count($deposits) > 0)
-                    @foreach($deposits as $deposit)
-                        <tr>
-                            <td class="success">{{ $deposit->plan->type->name }}</td>
-                            <td class="success">{{ $deposit->days }}</td>
-                            <td class="success">{{ $deposit->percent }} %</td>
-                            <td class="success">{{ $deposit->amount }}</td>
-                            <td class="success">{{ $deposit->total }}</td>
-                            @if($deposit->status == 0)
-                                <td class="success">Ожидайте...</td>
-                            @else
-                                <td class="success">Выполнена</td>
-                            @endif
-                            <td class="success">{{ $deposit->conclusion->format('d.m.Y H:i:s') }}</td>
-                            <td class="success">{{ $deposit->created_at->diffForHumans() }}</td>
+                <div class="table-responsive">
+                    <table class="table table-bordered" style="margin-top: 30px;">
+                        <thead>
+                        <tr class="head">
+                            <th>Тип вклада</th>
+                            <th>Дней</th>
+                            <th>Процент</th>
+                            <th>Сумма вклада EL</th>
+                            <th>Сумма для вывода EL</th>
+                            <th>Статус</th>
+                            <th>Истечение времени вклада</th>
+                            <th>Создан</th>
                         </tr>
-                    @endforeach
+                        </thead>
+                        <tbody>
+                        @if(count($deposits) > 0)
+                            @foreach($deposits as $deposit)
+                                <tr>
+                                    <td class="success">{{ $deposit->plan->type->name }}</td>
+                                    <td class="success">{{ $deposit->days }}</td>
+                                    <td class="success">{{ $deposit->percent }} %</td>
+                                    <td class="success">{{ $deposit->amount }}</td>
+                                    <td class="success">{{ $deposit->total }}</td>
+                                    @if($deposit->status == 0)
+                                        <td class="success">Ожидайте...</td>
+                                    @else
+                                        <td class="success">Выполнена</td>
+                                    @endif
+                                    <td class="success">{{ $deposit->conclusion->format('d.m.Y H:i:s') }}</td>
+                                    <td class="success">{{ $deposit->created_at->diffForHumans() }}</td>
+                                </tr>
+                            @endforeach
                         @else
                             <p class="h3 text-center">Нет вкладов</p>
                         @endif
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         </div>
 
