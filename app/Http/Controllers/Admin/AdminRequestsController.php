24299590f0dcd3ed22e803b300e5a3272be8c646
Lyovkin <anatoliymolinari@gmail.com>
2016-07-20 16:11:17 +0300

    fix

--- app/Http/Controllers/Admin/AdminRequestsController.php
+++ app/Http/Controllers/Admin/AdminRequestsController.php
@@ -48,14 +48,11 @@ class AdminRequestsController extends AbstractAdminController
 
         } elseif (\Auth::user()->hasRole('admin')) {
 
-            if($purchase->status_trust == 0) {
+            if($purchase->status_trust == 0 && !$purchase->type_id == 3) {
                 $bank = Banks::where('banks_profiles_id', 1)->first();
 
                 $bank->amount -= $purchase->total;
                 $bank->update();
-
-                $user->balance += $purchase->total;
-                $user->update();
 
             } elseif ($purchase->status_trust == 1) {
                 $bank = Banks::where('banks_profiles_id', 2)->first();
